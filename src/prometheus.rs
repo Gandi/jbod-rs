417437b193fafd8b40e4711d4b48f62c4212c722
Axel Viala <axel.viala@darnuria.eu>
2022-07-28 21:08:10 +0200

    prometheus: use unwrap_or_else

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -130,26 +130,20 @@ async fn metrics_handler() -> Result<impl Reply, Rejection> {
         eprintln!("could not encode custom metrics: {}", e);
     };
 
-    let mut res = match String::from_utf8(buffer.clone()) {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("custom metrics could not be from_utf8: {}", e);
-            String::default()
-        }
-    };
+    let mut res = String::from_utf8(buffer.clone()).unwrap_or_else(|e| {
+        eprintln!("custom metrics could not be from_utf8: {}", e);
+        String::default()
+    });
     buffer.clear();
 
     let mut buffer = Vec::new();
     if let Err(e) = encoder.encode(&prometheus::gather(), &mut buffer) {
         eprintln!("could not encode prometheus metrics: {}", e);
     };
-    let res_custom = match String::from_utf8(buffer.clone()) {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("prometheus metrics could not be from_utf8'd: {}", e);
-            String::default()
-        }
-    };
+    let res_custom = String::from_utf8(buffer.clone()).unwrap_or_else(|e| {
+        eprintln!("prometheus metrics could not be from_utf8'd: {}", e);
+        String::default()
+    });
     buffer.clear();
 
     res.push_str(&res_custom);
