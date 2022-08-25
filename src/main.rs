093b9c15249ed45c5021ffb4238e08d201862db7
Axel Viala <axel.viala@gandi.net>
2022-08-25 11:20:11 +0200

    subcommand list: Fan option is exclusive.

--- src/main.rs
+++ src/main.rs
@@ -214,6 +214,7 @@ fn main() {
                         .multiple(false)
                         .required(false)
                         .takes_value(false)
+                        .exclusive(false)
                         .help("List fan"),
                 ),
         )
