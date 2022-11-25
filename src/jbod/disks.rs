Gandi/jbod-rs

commit f62e2718c965aa18c2798f49163d61f24931a01c

get_disk_details: cargo fmt + use format!(). (#11)
Also removed a `to_owned()` call.

src/jbod/disks.rs
@@ -374,7 +374,18 @@ pub mod DiskShelf {
     fn get_disk_details(
         device: String,
         enclosure_slot: String,
-    ) -> (String, String, String, String, String, String, String, String, String, String) {
+    ) -> (
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+        String,
+    ) {
         let sys_class_enclosure: &str = "/sys/class/enclosure/";
         let mut enclosure = String::new();
         let mut slot = String::new();
@@ -388,19 +399,16 @@ pub mod DiskShelf {
         let mut disk_fault_led = String::new();
 
         let path_tostr_spl: Vec<&str> = device.split('/').collect();
-        let _slot = path_tostr_spl[5].to_owned();
+        let _slot = path_tostr_spl[5];
         let cmp_slot = _slot.to_lowercase();
         if cmp_slot.contains("slot")
             || cmp_slot.contains("disk")
             || cmp_slot.contains("array device")
             || cmp_slot.bytes().all(|c| c.is_ascii_digit())
         {
-            let generic_device = sys_class_enclosure.to_string()
-                + &enclosure_slot
-                + "/"
-                + &_slot
-                + "/device";
-            let physical_device = generic_device.clone() + "/scsi_generic/";
+            let generic_device = format!("{sys_class_enclosure}{enclosure_slot}/{_slot}/{device}");
+            let physical_device =  format!("{generic_device}/scsi_generic/");
+
             if Util::path_exists(&physical_device) {
                 let physical_path = fs::read_dir(physical_device).unwrap();
                 for dev in physical_path {
@@ -410,18 +418,18 @@ pub mod DiskShelf {
                     let __get_slot: Vec<&str> = split_dev[5].split(',').collect();
                     enclosure = split_dev[4].to_string();
                     slot = __get_slot[0].to_string();
-                    device_path = "/dev/".to_string() + &split_dev[8].to_string();
+                    device_path = format!("/dev/{}", split_dev[8]);
                     temperature = get_disk_temperature(device_path.clone());
                     fw_revision = get_disk_firmware(device_path.clone());
                     vendor = get_disk_vendor(generic_device.clone().to_string());
                     model = get_disk_model(generic_device.clone().to_string());
                     serial = get_disk_serial(generic_device.clone().to_string());
-                    disk_locate_led = get_disk_led_locate_path(&enclosure_slot, &split_dev[5]);
-                    disk_fault_led = get_disk_led_fault_path(&enclosure_slot, &split_dev[5]);
+                    disk_locate_led = get_disk_led_locate_path(&enclosure_slot, split_dev[5]);
+                    disk_fault_led = get_disk_led_fault_path(&enclosure_slot, split_dev[5]);
                 }
             }
         }
-        return (
+        (
             enclosure,
             slot,
             device_path,
@@ -432,7 +440,7 @@ pub mod DiskShelf {
             serial,
             disk_locate_led,
             disk_fault_led,
-        );
+        )
     }
 
     /// Returns a vector of disk structure

commit 759eb8ba1efafe004c7c1e6a1d5c782388deab0a

Fix formating issue in get_disk_detail.
Bug was introduced in f62e271 by myself, accidentally changed from `/device` to
`/{device}`. `/device` need to be verbatim in path and not interpreted as device_name.

src/jbod/disks.rs
@@ -406,7 +406,7 @@ pub mod DiskShelf {
             || cmp_slot.contains("array device")
             || cmp_slot.bytes().all(|c| c.is_ascii_digit())
         {
-            let generic_device = format!("{sys_class_enclosure}{enclosure_slot}/{_slot}/{device}");
+            let generic_device = format!("{sys_class_enclosure}{enclosure_slot}/{_slot}/device");
             let physical_device =  format!("{generic_device}/scsi_generic/");
 
             if Util::path_exists(&physical_device) {